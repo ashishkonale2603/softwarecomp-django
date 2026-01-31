1d05cec1b4b625a3c4e0822d7698003146ab387a
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-31 21:39:00 +0530

    urls-setup

---
+++ web/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from web import views
+
+urlpatterns = [
+    path("", views.index, name='home'),
+    path("services", views.services, name='services'),
+    path("product", views.product, name='product'),
+    path("contact", views.contact, name='contact'),
+    path("about", views.about, name='about')
+
+]
---
+++ web/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("This is home page")
+
+def services(request):
+    return HttpResponse("This is services page")
+
+def product(request):
+    return HttpResponse("This is product page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
+
+def about(request):
+    return HttpResponse("This is about page")
